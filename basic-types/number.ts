bef34618bfa281b04ce3d50f26fa63a56233c1a5
space086 <space_086@naver.com>
2022-02-09 11:51:12 +0900

    ts-number

---
+++ basic-types/number.ts
@@ -0,0 +1,22 @@
+//number
+//- javascript와 같이 typescript의 모든 숫자는 부동 소숫점 값
+//- typescript는 16진수 및 10진수 리터럴 외에도, 
+//  ECMAScript 2015에 도입된 2진수 및 8진수를 지원
+//- NaN
+//- 1_000_000 과 같은 표기 가능
+
+// 십진수
+let decimal:number = 6;
+
+// 십육진수
+let hex: number = 0xf00d;
+
+// 이진수
+let binary:number = 0b1010;
+
+// 팔진수
+let octal: number = 0o744;
+
+let notANumer: number = NaN;
+
+let underscoreNum:number = 1_000_000;
