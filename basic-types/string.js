a36d5902ce104ae95d4f0fbcdf617be27a47f717
space086 <space_086@naver.com>
2022-02-10 13:05:04 +0900

    ts-string+symbol

---
+++ basic-types/string.js
@@ -0,0 +1,17 @@
+"use strict";
+//string
+// 다른 언어에서와 마찬가지로 텍스트 형식을 참조하기 위해 string 형식을 사용
+// javascript와 마찬가지로, typescript는 문자열 데이터를
+// 둘러싸기 위해 큰 따옴표나 작은 따옴표를 사용.
+let myName = "Mark";
+myName = "Anna";
+// template String
+// - 행에 걸쳐있거나 , 표현식을 넣을 수 있는 문자열
+// - 이 문자열은 backtick(=backquote) 기호에 둘러쌓여 있습니다.
+// - 포함된 표현식은 `${expr}` 와 같은 형태로 사용
+let fullName = "Mark Lee";
+let age = 39;
+let sentence = `hello my name is ${fullName}.
+
+I'll be ${age + 1} years old next month.`;
+console.log(sentence);
---
+++ basic-types/symbol.js
@@ -0,0 +1,6 @@
+"use strict";
+//symbol
+// ECMAScript 2015의 symbol 입니다.
+// new Symbol 로 사용할 수 없습니다.
+// symbol 을 함수로 사용해서 symbol 타입을 만들어낼 수 있습니다.
+console.log(Symbol("foo") === Symbol("foo"));
