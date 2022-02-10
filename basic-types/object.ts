21f4d99f2bdc4958dd9b120d40b7f939502dd1f5
space086 <space_086@naver.com>
2022-02-10 14:39:25 +0900

    ts-object
    basic-type

---
+++ basic-types/object.ts
@@ -0,0 +1,10 @@
+//object
+
+//object literal 타입
+const person1 = { name: "mark", age: 39 };
+
+// Object.create 타입
+const person2 = Object.create({ name: "Mark" , age: 39 });
+
+// object
+// 프리미티브 타입이 아닌 것을 나타내고 싶을 떄 사용.
