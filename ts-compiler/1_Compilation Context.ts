9715a01e6fe4831ab1c88bf0c0ce77d0e063389a
space086 <space_086@naver.com>
2022-02-15 17:42:53 +0900

    ts-compiler

---
+++ ts-compiler/1_Compilation Context.ts
@@ -0,0 +1,16 @@
+
+/*
+컴파일 컨텍스트는 근본적으로 집단에서 사용하는 용어이며 타입스크립트는 유효한 것을 분석하고 해석합니다. 어떤 파일의 정보와 함께 컴파일 컨텍스트에는 사용중인 컴파일러 옵션에 대한 정보가 들어있습니다. 이 논리그룹을 정의하는 가장 좋은 방법은 tsconfig.json 파일을 사용하는 것 입니다.
+
+컴파일할 파일, 컴포넌트 등의 모음입니다. 이것이 의미하는 것은 해당 컨텍스트에는 컴파일러가 어떠한 에러도 없이 컴파일 하는데 필요한 모든 것을 포함하고 있다는 것입니다.
+
+typescript 를 컴파일할 때, 여러분은 컨텍스트를 files, include 또는 exclude 옵션을 사용한 tsconfig.json 파일을 이용하여 제어합니다. 이 방식의 typescript 컴파일러는 여러분의 ts 코드간의 관계를 찾을 때 여러분이 제공한 파일들만 사용할 것 입니다.
+
+
+출처 : https://radlohead.gitbook.io/typescript-deep-dive/project/compilation-context
+
+출처 : https://rottk.tistory.com/entry/
+%EC%BB%B4%ED%95%84%EB%A0%88%EC%9D%B4%EC%85%98-%EC%BB%A8%ED%85%8D%EC%8A%A4%ED%8A%B8-Compilation-context
+
+
+*/
